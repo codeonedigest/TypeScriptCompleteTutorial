50e405b523187942e3ef52fbe29ba5764ec13f98
codeonedigest <codeonedigest@gmail.com>
2023-10-28 13:03:09 +0530

    Restructured the projects

--- Lesson1 - TypeScript Introduction Installation Project Setup & Basic Types/index.js
+++ Lesson1 - TypeScript Introduction Installation Project Setup & Basic Types/index.js
@@ -1,3 +1,2 @@
 var firstName = "Dylan"; // type string
-firstName = 32;
 console.log(typeof firstName);
--- Lesson1 - TypeScript Introduction Installation Project Setup & Basic Types/index.ts
+++ Lesson1 - TypeScript Introduction Installation Project Setup & Basic Types/index.ts
@@ -1,3 +1,2 @@
 let firstName: string = "Dylan"; // type string
-firstName = 32;
 console.log(typeof firstName);
